341f1c6ece3b0b4912deb36030ec42c8f7dc8360
KamilRizatdinov <k.rizatdinov@innopolis.university>
2022-03-14 23:42:49 +0300

    test: add bookmark test cases

--- src/components/Bookmark/__tests__/index.jsx
+++ src/components/Bookmark/__tests__/index.jsx
@@ -3,7 +3,7 @@
  */
 
 import React from 'react'
-import { cleanup, render } from '@testing-library/react'
+import { cleanup, render, fireEvent } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import Bookmark from '../index'
 import mockStore from '../../../__mocks__/store'
@@ -15,7 +15,7 @@ beforeEach(() => {
 afterEach(cleanup)
 
 describe('Bookmark', () => {
-  it('Render off', () => {
+  it('Render "off"', () => {
     const { container } = render(
       <Provider store={store}>
         <Bookmark bookmark={false} id="1" />
@@ -25,13 +25,37 @@ describe('Bookmark', () => {
     expect(container).toMatchSnapshot()
   })
 
-  it('Render on', () => {
+  it('Render "on"', () => {
     const { container } = render(
       <Provider store={store}>
         <Bookmark bookmark id="1" />
       </Provider>
     )
 
+    expect(container).toMatchSnapshot()
+  })
+
+  it('After click bookmark should be "on"', () => {
+    const { container, getByTestId } = render(
+      <Provider store={store}>
+        <Bookmark bookmark={false} id="1" />
+      </Provider>
+    )
+
+    expect(container).toMatchSnapshot()
+    fireEvent.click(getByTestId('bookmark'))
+    expect(container).toMatchSnapshot()
+  })
+
+  it('After click bookmark should be "off"', () => {
+    const { container, getByTestId } = render(
+      <Provider store={store}>
+        <Bookmark bookmark id="1" />
+      </Provider>
+    )
+
+    expect(container).toMatchSnapshot()
+    fireEvent.click(getByTestId('bookmark'))
     expect(container).toMatchSnapshot()
   })
 })
